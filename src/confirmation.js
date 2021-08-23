45e05ee6694f31f17febf48a6b2da6aca5abd856
Jonathan Ribiere <ribiere88@sfr.fr>
2021-08-23 15:17:40 +0200

    mcommit apres session mentor

--- src/confirmation.js
+++ src/confirmation.js
@@ -1,26 +1,34 @@
-const reponse = JSON.parse(localStorage.getItem("order"));
-console.log(reponse);
+const order = JSON.parse(localStorage.getItem("order"));
 
-const orderId = localStorage.getItem("reponse.contact");
-console.log(`orderId : ${reponse.orderId}`);
+const calculatePriceTotal = () => {
+  let priceTotal = 0;
+  order.products.map((product) => (priceTotal += product.price));
+  // EQUIVALENT AU MAP
+  // for (let i = order.products.length; i--;) {
+  //   priceTotal += order.products[i].price;
+  // }
+  return priceTotal;
+};
+
+const priceTotal = calculatePriceTotal();
 
 const positionElementConfirm = document.querySelector(
   "#containerRecapitulatif"
 );
 const structureConfirmCommande = `
 <h2 id="confirmation-h2">
-Félicitations ${reponse.contact.firstName}, votre commande a était realisée avec succès
+Félicitations ${order.contact.firstName}, votre commande a était realisée avec succès
 </h2>
 <div id="recapCommande">
-        <p>Merci ${reponse.contact.firstName} ${reponse.contact.lastName} pour votre commande.</p>
+        <p>Merci ${order.contact.firstName} ${order.contact.lastName} pour votre commande.</p>
         <p>
-          Votre identifiant de commande :<br>${reponse.orderId}<br>est bien
+          Votre identifiant de commande :<br>${order.orderId}<br>est bien
           prise en compte.
         </p>
         <p>
-          Le montant de votre commande est de : <span class="gras">xxxxxx</span>
+          Le montant de votre commande est de : ${priceTotal}
         </p>
-        <p>Vous serait livrée au ${reponse.contact.address} à ${reponse.contact.city} dans les meilleurs délais.</p>
+        <p>Vous serait livrée au ${order.contact.address} à ${order.contact.city} dans les meilleurs délais.</p>
       </div>
 `;
 // Injection du html
