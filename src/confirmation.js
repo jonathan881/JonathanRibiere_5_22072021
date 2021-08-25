9352fced8bea4f7b5d9b0de9dcd54c8faa55a3dc
Jonathan Ribiere <ribiere88@sfr.fr>
2021-08-25 19:39:12 +0200

    Amelioration du css global

--- src/confirmation.js
+++ src/confirmation.js
@@ -15,27 +15,29 @@ const priceTotal = calculatePriceTotal();
 const positionElementConfirm = document.querySelector(
   "#containerRecapitulatif"
 );
+
 const structureConfirmCommande = `
 <h2 id="confirmation-h2">
-Félicitations ${
-  order.contact.firstName
-}, votre commande a était realisée avec succès
+Félicitations ${order.contact.firstName}, votre commande a était
+realisée avec succès
 </h2>
 <div id="recapCommande">
-        <p>Merci ${order.contact.firstName} ${
-  order.contact.lastName
-} pour votre commande.</p>
-        <p>
-          Votre identifiant de commande :<br>${order.orderId}<br>est bien
-          prise en compte.
-        </p>
-        <p>
-        Le montant de votre commande est de : ${(priceTotal / 100).toFixed(2)}€
-        </p>
-        <p>Vous serait livrée au ${order.contact.address} à ${
-  order.contact.city
-} dans les meilleurs délais.</p>
-      </div>
+<p class="p-confirmation">
+  Votre identifiant de commande :<br />${order.orderId}<br />est bien
+  prise en compte.
+</p>
+<p class="p-confirmation">
+  Le montant de votre commande est de : ${(priceTotal / 100).toFixed(2)}€
+</p>
+<p class="p-confirmation">
+  Vous serait livrée au ${order.contact.address} à ${order.contact.city}
+  dans les meilleurs délais.
+</p>
+<p class="p-confirmation">
+  Merci ${order.contact.firstName} ${order.contact.lastName} pour votre
+  commande.
+</p>
+</div>
 `;
 // Injection du html
 positionElementConfirm.innerHTML = structureConfirmCommande;
